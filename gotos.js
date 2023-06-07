88e13d8aa9c9505da6860630cc4d8296d73e5b53
EnityBlackHawk <luanf2003@gmail.com>
2023-06-07 16:35:01 -0300

    Update descricao da acch.pdf, gotos.js, and 15 more files...

--- gotos.js
+++ gotos.js
@@ -1,9 +1,25 @@
-function gotoMarkwork()
-{
-      window.location.href = "produtos/MarketWork.html";
+function gotoMarkwork() {
+  window.location.href = "produtos/MarketWork.html";
 }
 
-function gotoOrcamento()
+function gotoOrcamento() {
+  window.location.href = "../orcamento.html";
+}
+
+function gotoModelationWorks()
 {
-      window.location.href = "../orcamento.html";
+      window.location.href = "produtos/ModelationWork.html";
+}
+
+function gotoEngineWorks()
+{
+  window.location.href = "produtos/EngineWorks.html";
+}
+
+function gotoLove(e) {
+  let t = document.querySelector("form input[type='text']").value;
+  if (t == "Miriam" || t == "Miriam eu te amo") {
+    e.preventDefault();
+    window.location.href = "produtos/love.html";
+  }
 }
--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var root = document.querySelector(':root')
 
+var menu = document.querySelector('nav');
+var menuOpts = document.querySelector("nav ul");
+var isOpen = true;
+
 var title = document.querySelector('.title');
 root.style.setProperty('--headerSize', title.offsetHeight + 'px');
 console.log(title.offsetHeight);
@@ -7,4 +11,21 @@ console.log(title.offsetHeight);
 
 function gotoProdutos() {
   window.location.href = "pages/produtos.html";
+}
+
+function toggleMenu()
+{
+  if(isOpen == true)
+  {
+    menuOpts.style.display = "none";
+    menu.style.height = title.offsetHeight + 'px';
+    isOpen = false;
+  }
+
+  else
+  {
+    menuOpts.style.display = "block";
+    menu.style.height = "auto";
+    isOpen = true;
+  }
 }
